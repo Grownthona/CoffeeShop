2c5978ab13c6b55e70c14db977205dd449c47d80
Grownthona Rahman <rahmangronthona@outlook.com>
2023-08-13 17:14:41 +0600

    new

--- client/src/components/Products/Product.js
+++ client/src/components/Products/Product.js
@@ -37,7 +37,7 @@ export default function Product(){
       useEffect(() => {
         const fetchProduct = async () => {
             try {
-              const response = await axios.get(`https://dough-bakery.vercel.app/products`, {
+              const response = await axios.get(`/products`, {
                 withCredentials: true
               });
           
